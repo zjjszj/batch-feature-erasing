e77b2b84944649c74b43c2e57f161f96b7736fd2
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-04-16 11:03:25 +0800

    add center loss

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class DefaultConfig(object):
     loss = 'triplet+center'  #triplet
     optim = 'adam'
     max_epoch = 60
-    train_batch = 1
+    train_batch = 2
     test_batch = 32
     adjust_lr = False
     lr = 0.0001
