02e3d90aee3c2d516ae4e021f7a08e4814a34c27
zhang jin <hblfgazj@163.com>
2020-08-21 18:13:54 +0800

    trian a new model only has 10 classes.

--- datasets/data_manager.py
+++ datasets/data_manager.py
@@ -19,7 +19,7 @@ class Market1501(object):
     # identities: 1501 (+1 for background)
     # images: 12936 (train) + 3368 (query) + 15913 (gallery)
     """
-    def __init__(self, dataset_dir, mode, root='/kaggle/input/market1501'):
+    def __init__(self, dataset_dir, mode, root='/kaggle/input/market1501/Market1501'):
         self.dataset_dir = dataset_dir
         self.dataset_dir = osp.join(root, self.dataset_dir)
         self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
@@ -66,7 +66,7 @@ class Market1501(object):
             raise RuntimeError("'{}' is not available".format(self.gallery_dir))
 
     def _process_dir(self, dir_path, relabel=False):
-        if dir_path.split('/')[-1]=="bounding_box_train":
+        if dir_path.split('\\')[-1]=="bounding_box_train":
             img_names = os.listdir(dir_path)[:196]
         img_paths = [os.path.join(dir_path, img_name) for img_name in img_names \
             if img_name.endswith('jpg') or img_name.endswith('png')]
